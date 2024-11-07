12a04843036ccd48e4e59ef51fa4e5e2ca0896f8
payne <harrison.payne@gmail.com>
2024-11-07 16:20:30 -0700

    Message for no args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("No arguments passed. Please pass name of file to read in.");
+    } else {
+        // TODO: read in file, construct data, process data, print result
+    }
 }
